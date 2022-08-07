9f01773f09fe95a1fd12f3a01f1e8e0b20809244
Liezel <liezelarmilla061698@gmail.com>
2022-08-07 14:21:17 +0800

    My first commit.

---
+++ main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Odd NUmber
+//
+//  Created by Liezel Mae Armilla on 11/9/21.
+//
+#include <stdio.h>
+
+int main()
+{
+    
+    int number1;
+    printf("\n Enter a number:");
+    scanf(" %d", &number1);
+    if (number1%2==0)
+        printf ("\n The number is Even\n");
+    else
+        printf ("\n The number is Odd\n");
+    
+
+}
